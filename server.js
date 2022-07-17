67e8e4abf3d52eed755d20a3d0db7f77b295cb8e
iprabhjeet9 <iprabhjeet9@gmail.com>
2022-07-17 19:34:06 +0530

    added new func

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var os = require('os')
+var ip = require('ip')
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello There !! hostname: '+ os.hostname() +"\n ip: "+ip.address());
+
 });
 app.listen(3000, function () {
     console.log('Listening on port 3000!');
